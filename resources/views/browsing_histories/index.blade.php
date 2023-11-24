a6cf895aba98c398520518db39d40d33d6cdc72e
borambence <boramiklosbence@gmail.com>
2023-11-24 20:36:44 +0100

    ads

--- resources/views/browsing_histories/index.blade.php
+++ resources/views/browsing_histories/index.blade.php
@@ -14,7 +14,7 @@
         {{-- Browsing histories --}}
         <div class="row justify-content-center mb-2">
             <div class="col-md-10 col-lg-8">
-                @foreach($extended_browsing_histories as $extended_browsing_history)
+                @forelse ( $extended_browsing_histories as $extended_browsing_history)
                     <div class="card mb-2 hover">
                         <div class="card-body d-flex align-items-center">
                             <div class="col-3">
@@ -28,13 +28,23 @@
                             <div class="col-3 text-center">{{$extended_browsing_history['browsing_history']['searched_registration_number']}}</div>
                             <div class="col-3 text-center">{{$extended_browsing_history['browsing_history']['searched_at']}}</div>
                             <div class="col-3 text-center">
-                                <a href="#" role="button" class="btn btn-primary">
-                                    Megtekintés
-                                </a>
+                                <form action="{{ route('browsing_histories.store') }}" method="POST" class="hidden-form">
+                                    @csrf
+                                    <input type="hidden" id="registration_number" name="registration_number" value="{{ $extended_browsing_history['vehicle']['registration_number']}}">
+                                    <button type="submit" class="btn btn-primary">
+                                        Megtekintés
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </div>
-                @endforeach
+                @empty
+                    <div class="col-md-10 col-lg-8">
+                        <div class="alert alert-primary" role="alert">
+                            Nincs megjeleníthető keresési előzmény.
+                        </div>
+                    </div>
+                @endforelse
             </div>
         </div>
     </div>
